02b6692d124e417845cace817cf59906a8b4cc03
hovanco <hovanco.vn@gmail.com>
2024-12-31 10:16:34 +0700

    Config source

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+// config default
+// import type { NextConfig } from "next";
+// const nextConfig: NextConfig = {};
+// export default nextConfig;
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  devIndicators: {
+    appIsrStatus: false,
+  },
 };
 
 export default nextConfig;
